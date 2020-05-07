bdc6b1dcdeec4e2e1c863b7d2d46eaa02e7e6229
Naralha <vitorasr22@gmail.com>
2020-05-07 15:30:15 -0300

    Criacao da funcionalidade da arvore no back

--- src/main/java/camada/entidade/Organograma.java
+++ src/main/java/camada/entidade/Organograma.java
@@ -1,8 +1,9 @@
 package camada.entidade;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import javax.persistence.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,12 +14,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-
 import camada.dao.Dao;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Getter
 @Setter
@@ -31,7 +30,7 @@ public class Organograma extends Dao{
 	@PrimaryKeyJoinColumn
 	@Column(name = "id_nVarOrganograma", nullable = false)
 	private long id;
-
+	
 	@ManyToOne
 	@JoinColumn(name="id_nVarEmpresa")
 	private Empresa empresa;
@@ -42,59 +41,51 @@ public class Organograma extends Dao{
 	@Column(name = "nVarDescricao", nullable = false)
 	private String descricao;
 	
-	@Column(name = "id_nVarPaiOrganograma")
-	private Long idPaiOrganograma;
-
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "tbFuncionarioOrganograma",
-			joinColumns = @JoinColumn(name="id_nVarOrganograma"),
-			inverseJoinColumns = @JoinColumn(name="id_nVarFuncionario"))
+	@Column(name = "id_nVarPaiOrganograma", nullable = false)
+	private long idPaiOrganograma;
+	
+	//montar relacionamento com tabela usuario
+	@Column(name = "id_nvarUsuario", nullable = false)
+	private String idUsuario;
+	
+	@ManyToMany(mappedBy = "organogramas")
 	private Set<Funcionario> funcionarios;
 	
+//	@OneToMany(mappedBy = "organograma", targetEntity = Processo.class, fetch = FetchType.LAZY)
+//	private Processo processo;
+	
 	public void salvar() {
+			
 		iniciarOperacao();
-		this.setId(getSequence(this.descricao));
+		
+		setId(sequence());
 		session.save(this);
+		
 		finalizarOperacao();
-	}
-
+		
+	} 
+	
 	public List<Organograma> find(){
+			
 		iniciarOperacao();
-		List<Organograma> listaOrganogramas;
+		
+		List<Organograma> listaOrganogramas = new ArrayList<Organograma>();
 		listaOrganogramas = session.createQuery("SELECT a FROM Organograma a", Organograma.class).getResultList();
+		
 		finalizarOperacao();
-
+		
 		return listaOrganogramas;
 	}
 
-	public List findOrganogramaPorEmpresa(long idEmpresa) {
+	public List<Organograma> findOrganogramaPorEmpresa() {
+
 		iniciarOperacao();
-		Query query = session.createSQLQuery(
-				"WITH organogramaEmpresa\n" +
-				"          AS ( SELECT pai.id_nVarOrganograma, pai.id_nVarEmpresa,\n" +
-				"                     pai.nVarNome, pai.nVarDescricao, pai.id_nVarPaiOrganograma,\n" +
-				"              1 AS OrganogramaLevel\n" +
-				"              FROM tbOrganograma as pai\n" +
-				"              WHERE pai.id_nVarPaiOrganograma IS NULL\n" +
-				"              AND pai.id_nVarEmpresa = (:idEmpresa)\n" +
-				"\n" +
-				"              UNION ALL\n" +
-				"\n" +
-				"              SELECT filho.id_nVarOrganograma, filho.id_nVarEmpresa,\n" +
-				"                     filho.nVarNome, filho.nVarDescricao, filho.id_nVarPaiOrganograma,\n" +
-				"              tree.OrganogramaLevel + 1\n" +
-				"              FROM tbOrganograma as filho\n" +
-				"              INNER JOIN organogramaEmpresa as tree\n" +
-				"              ON filho.id_nVarPaiOrganograma = tree.id_nVarOrganograma\n" +
-				"              WHERE filho.id_nVarPaiOrganograma IS NOT NULL\n" +
-				"              AND filho.id_nVarEmpresa = (:idEmpresa)\n" +
-				"            )\n" +
-				"SELECT *\n" +
-				"FROM organogramaEmpresa;")
-				.setParameter("idEmpresa", idEmpresa);
-		List resultList = query.getResultList();
+		
+		List<Organograma> listaOrganograma = session.createQuery("SELECT a FROM Organograma a Where a.empresa ='"+ this.empresa.getId()+"'", Organograma.class).getResultList();
+		
 		finalizarOperacao();
-		return resultList;
+		
+		return listaOrganograma;
 	}
 
 	public String iniciarDelete() {
@@ -105,11 +96,12 @@ public class Organograma extends Dao{
 		List<Organograma> listaOrganogramas = new ArrayList<Organograma>();
 		listaOrganogramas = session.createQuery("SELECT a FROM Organograma a where a.idPaiOrganograma ='"+ this.id+"'", Organograma.class).getResultList();
 		
+		//Se tiver filho, o usuario deve alterar na mão.
 		if(listaOrganogramas.isEmpty()){
 			Organograma organograma = (Organograma)session.load(Organograma.class, this.id);
 			
+//			o usuário tem a chance de escolher se quer deletar mesmo com funcionario associado.
 			if(!organograma.getFuncionarios().isEmpty()) {
-				
 				finalizarOperacao();
 				return "Existe funcionario associado.";
 			}
@@ -139,8 +131,29 @@ public class Organograma extends Dao{
 	}
 
 	public void atualizar() {
+		
 		iniciarOperacao();
+		
 		session.update(this);
+		
 		finalizarOperacao();
+		
+	} 
+	
+	public String montarArvore() {
+		StringBuilder arvore = new StringBuilder("[");
+		List<Organograma> listaOrganograma = findOrganogramaPorEmpresa();
+		if(!listaOrganograma.isEmpty()) {
+			Iterator<Organograma> it = listaOrganograma.iterator();
+			for(Organograma o : listaOrganograma){
+				it.next();
+				arvore.append("{\"id\":"+o.getId()+",\"text\":"+o.getNome()+",\"parent\":"+o.getIdPaiOrganograma()+"}");
+				if(it.hasNext()) {
+					arvore.append(",");
+				}
+			}
+		}
+		arvore.append("]");
+		return arvore.toString();
 	}
 }
--- src/src/main/java/camada/entidade/Organograma.java
+++ src/src/main/java/camada/entidade/Organograma.java
@@ -1,6 +1,7 @@
 package camada.entidade;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -43,13 +44,21 @@ public class Organograma extends Dao{
 	@Column(name = "id_nVarPaiOrganograma", nullable = false)
 	private long idPaiOrganograma;
 	
+	//montar relacionamento com tabela usuario
+	@Column(name = "id_nvarUsuario", nullable = false)
+	private String idUsuario;
+	
 	@ManyToMany(mappedBy = "organogramas")
 	private Set<Funcionario> funcionarios;
+	
+//	@OneToMany(mappedBy = "organograma", targetEntity = Processo.class, fetch = FetchType.LAZY)
+//	private Processo processo;
 	
 	public void salvar() {
 			
 		iniciarOperacao();
-			
+		
+		setId(sequence());
 		session.save(this);
 		
 		finalizarOperacao();
@@ -66,5 +75,85 @@ public class Organograma extends Dao{
 		finalizarOperacao();
 		
 		return listaOrganogramas;
+	}
+
+	public List<Organograma> findOrganogramaPorEmpresa() {
+
+		iniciarOperacao();
+		
+		List<Organograma> listaOrganograma = session.createQuery("SELECT a FROM Organograma a Where a.empresa ='"+ this.empresa.getId()+"'", Organograma.class).getResultList();
+		
+		finalizarOperacao();
+		
+		return listaOrganograma;
+	}
+
+	public String iniciarDelete() {
+		
+		iniciarOperacao();
+		
+		//validacao se tem relacionamento com id_pai
+		List<Organograma> listaOrganogramas = new ArrayList<Organograma>();
+		listaOrganogramas = session.createQuery("SELECT a FROM Organograma a where a.idPaiOrganograma ='"+ this.id+"'", Organograma.class).getResultList();
+		
+		//Se tiver filho, o usuario deve alterar na mão.
+		if(listaOrganogramas.isEmpty()){
+			Organograma organograma = (Organograma)session.load(Organograma.class, this.id);
+			
+//			o usuário tem a chance de escolher se quer deletar mesmo com funcionario associado.
+			if(!organograma.getFuncionarios().isEmpty()) {
+				finalizarOperacao();
+				return "Existe funcionario associado.";
+			}
+			else{
+				session.delete(organograma);
+				finalizarOperacao();
+				return "Deletado.";
+			}
+		}
+		
+		finalizarOperacao();
+		
+		return "Existe filho associado.";
+	}
+	
+	public boolean deletar() {
+		
+		iniciarOperacao();
+		
+		Organograma organograma = (Organograma)session.load(Organograma.class, this.id);
+			
+		session.createSQLQuery("Delete tbFuncionarioOrganograma where id_nVarOrganograma ='"+ this.id+"'").executeUpdate();
+		session.delete(organograma);
+			
+		finalizarOperacao();
+		return true;
+	}
+
+	public void atualizar() {
+		
+		iniciarOperacao();
+		
+		session.update(this);
+		
+		finalizarOperacao();
+		
 	} 
+	
+	public String montarArvore() {
+		StringBuilder arvore = new StringBuilder("[");
+		List<Organograma> listaOrganograma = findOrganogramaPorEmpresa();
+		if(!listaOrganograma.isEmpty()) {
+			Iterator<Organograma> it = listaOrganograma.iterator();
+			for(Organograma o : listaOrganograma){
+				it.next();
+				arvore.append("{\"id\":"+o.getId()+",\"text\":"+o.getNome()+",\"parent\":"+o.getIdPaiOrganograma()+"}");
+				if(it.hasNext()) {
+					arvore.append(",");
+				}
+			}
+		}
+		arvore.append("]");
+		return arvore.toString();
+	}
 }
