ecd938708110fa29b5bc2a7fd3d80a8b27faf936
Nunes <philippenunes@live.com>
2020-06-01 18:35:59 -0300

    Incluindo componente modal de funcionários.

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,18 +1,22 @@
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
+import { ModalEmployeeListComponent } from './modal-employee-list/modal-employee-list.component';
 
 @NgModule({
   declarations: [
     FormDebugComponent,
-    CampoControlErroComponent],
+    CampoControlErroComponent,
+    ModalEmployeeListComponent],
   imports: [
     CommonModule,
   ],
   exports: [
     FormDebugComponent,
     CampoControlErroComponent,
+    ModalEmployeeListComponent
   ]
 })
 export class SharedModule { }
